a5986e4e0dd6ac5d527a088bfe5280d5502ecb74
Adam Silver <adamsilver.ca@gmail.com>
2020-11-23 08:19:55 -0800

    first commit

---
+++ inventoy-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '07cf9ec079aafd298cc9b93663a192bf'),
+    },
+  },
+});
---
+++ inventoy-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ef378894-6b79-4151-a811-0d1afaf99535'
+};
